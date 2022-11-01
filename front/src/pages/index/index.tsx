1ae725f8fa2fe8e5ffba689aa85267867de19d9f
jdwns96 <djjng3@gmail.com>
2022-11-01 22:49:57 +0900

    [UPDATE] back

--- front/src/pages/index/index.tsx
+++ front/src/pages/index/index.tsx
@@ -1,3 +1,16 @@
+import { useAppDispatch, useAppSelector } from "src/store";
+
 export default function Home() {
-  return <div></div>;
+  const { id, user_id, name } = useAppSelector((store) => store.auth);
+
+  return (
+    <div>
+      <div>HOME</div>
+      <ul>
+        <li>{id ?? "null"}</li>
+        <li>{user_id ?? "null"}</li>
+        <li>{name ?? "null"}</li>
+      </ul>
+    </div>
+  );
 }
--- front/src/pages/signin/index.tsx
+++ front/src/pages/signin/index.tsx
@@ -1,18 +1,69 @@
-import React from "react";
+import { useMutation } from "@tanstack/react-query";
+import React, { useState } from "react";
+
+import client from "src/libs/axios";
+import { useAppDispatch } from "src/store";
+import { setAuth } from "src/store/auth";
+
+interface SigninPayload {
+  user_id: string;
+  password: string;
+}
 
 export default function Signin() {
+  const dispatch = useAppDispatch();
+
+  const [inputs, setInputs] = useState({
+    user_id: "",
+    password: "",
+  });
+
+  const { isLoading, isSuccess, isError, mutateAsync } = useMutation((payload: SigninPayload) =>
+    client
+      .post("/login", {
+        ...payload,
+      })
+      .then((res) => res.data),
+  );
+
+  const onChangeInputs = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputs((ps) => {
+      return {
+        ...ps,
+        [name]: value,
+      };
+    });
+  };
+
+  const onSignin = async () => {
+    try {
+      const { user_id, password } = inputs;
+      console.log(user_id, password);
+
+      const response = await mutateAsync({ user_id, password });
+      console.log(response);
+
+      dispatch(setAuth({ ...response.user })); // store
+      client.defaults.headers.authorization = `${response.token.accessToken}`; // access
+      localStorage.setItem("refresh", response.token.refreshToken); // refresh
+    } catch (e) {
+      console.dir(e);
+    }
+  };
+
   return (
     <div>
       <div>
         <span>아이디</span>
-        <input type="text" />
+        <input type="text" name="user_id" value={inputs.user_id} onChange={onChangeInputs} />
       </div>
       <div>
         <span>패스워드</span>
-        <input type="text" />
+        <input type="text" name="password" value={inputs.password} onChange={onChangeInputs} />
       </div>
       <div>
-        <button>signin</button>
+        <button onClick={onSignin}>signin</button>
       </div>
     </div>
   );
