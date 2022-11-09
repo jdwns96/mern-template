a2049667153e0faad8a7caacf5422ba6d26660fd
jdwns96 <djjng3@gmail.com>
2022-11-10 00:52:27 +0900

    [UPDATE] login page

--- front/src/pages/signin/index.tsx
+++ front/src/pages/signin/index.tsx
@@ -8,6 +8,7 @@ import client from "src/libs/axios";
 import { useAppDispatch } from "src/store";
 import { setAuth } from "src/store/auth";
 import toast, { Toaster } from "react-hot-toast";
+import { EggAlt } from "@mui/icons-material";
 
 interface SigninPayload {
   user_id: string;
@@ -19,8 +20,8 @@ export default function Signin() {
   const navigate = useNavigate();
 
   const [inputs, setInputs] = useState({
-    user_id: "",
-    password: "",
+    user_id: "foo",
+    password: "123",
   });
 
   const { isLoading, isSuccess, isError, mutateAsync } = useMutation((payload: SigninPayload) =>
@@ -67,25 +68,46 @@ export default function Signin() {
           <Paper className="p-8">
             <form action="" onSubmit={onSignin}>
               <div>
-                <input className="snack-input" name="user_id" value={inputs.user_id} onChange={onChangeInputs} />
+                <div className="py-0 ">
+                  <div className="py-4 px-4 flex justify-center items-center mb-4 bg-edge-blue rounded-md">
+                    <p className="text-snack-sky">
+                      <EggAlt style={{ width: 48, height: 48 }} />
+                    </p>
+                    <p className="font-extrabold ml-2 text-3xl text-snack-sky">SNACK</p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <input className="snack-input" name="password" value={inputs.password} onChange={onChangeInputs} />
+              <div className="mb-6">
+                <div className="mb-2">
+                  {/* <label htmlFor="" className="block text-gray-500 font-semibold  mb-1  pr-4 mt-3 text-sm">
+                    아이디 혹은 이메일
+                  </label> */}
+                  <input className="snack-input" name="user_id" value={inputs.user_id} onChange={onChangeInputs} placeholder="아이디 혹은 이메일" />
+                </div>
+                <div>
+                  {/* <label htmlFor="" className="block text-gray-500 font-semibold  mb-1  pr-4 mt-3 text-sm">
+                    비밀번호
+                  </label> */}
+                  <input className="snack-input" type="password" name="password" value={inputs.password} onChange={onChangeInputs} placeholder="비밀번호" />
+                </div>
               </div>
-              <div>
-                {isLoading === true ? (
-                  <button className="snack-btn" disabled onClick={onSignin}>
-                    <span>signin</span>
+              <div className="flex">
+                <div className="w-full mr-1">
+                  {isLoading === true ? (
+                    <button className="snack-btn w-full flex justify-center items-center" disabled onClick={onSignin}>
+                      <span>signin</span>
+                    </button>
+                  ) : (
+                    <button className="snack-btn w-full flex justify-center items-center" onClick={onSignin} type="submit">
+                      <span>signin</span>
+                    </button>
+                  )}
+                </div>
+                <div className="w-full ml-1">
+                  <button className="snack-btn  w-full flex justify-center items-center" type="button">
+                    <span>signup</span>
                   </button>
-                ) : (
-                  <button className="snack-btn" onClick={onSignin} type="submit">
-                    <span>signin</span>
-                  </button>
-                )}
-
-                <button className="snack-btn" type="button">
-                  <span>signup</span>
-                </button>
+                </div>
               </div>
             </form>
           </Paper>
